701c8ce585031db9dec2b7fc97931aed0c02a6bb
Maksim228337 <mvasilenko838@gmail.com>
2019-09-14 10:36:17 +0300

    update

---
+++ 1.py
@@ -0,0 +1,14 @@
+genre=dict()
+#Название фильмов через ,
+print('Your favourite horror films ')
+genre['Horror'] = input()
+print('Your favourite adventure films ')
+genre['Adventure'] = input()
+print('Your favourite science fiction films ')
+genre['Science fiction'] = input()
+print('Your favourite action films ')
+genre['Action'] = input()
+print('Your favourite comedy films ')
+genre['Comedy'] = input()
+print(genre)
+
