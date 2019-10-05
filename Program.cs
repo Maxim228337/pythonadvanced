a56eb1f9ee097b0be464cf8aae2ed81a755c92ea
Maksim228337 <mvasilenko838@gmail.com>
2019-10-05 14:23:28 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pythonadvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- zoopark.py
+++ zoopark.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-import random
-=======
+
 from random import randint
-b = {}
->>>>>>> 285c1c5461685b5bd3f7e6d46b778b041b9cbac2
+B = {}
 class Critter:
     """Виртуальный питомец"""
     total = 0
@@ -12,11 +9,7 @@ class Critter:
     def status():
         print("Общее число зверюшек", Critter.total)
 
-<<<<<<< HEAD
-    def __init__(self, name, hunger = 0, boredom = 0):
-=======
     def __init__(self, name, hunger = 0, boredom =0):
->>>>>>> 285c1c5461685b5bd3f7e6d46b778b041b9cbac2
         self.__name = name
         self.hunger = hunger
         self.boredom = boredom
@@ -38,8 +31,6 @@ class Critter:
             self.__name = new_name
             print("Имя успешно изменено.")
 
-    def talk(self):
-        print("Меня зовут", self.name)
     
 
 
@@ -60,45 +51,28 @@ class Critter:
         print("Меня зовут", self.name, ", и сейчас я чувствую себя", self.mood)
         self.__pass_time()
 
-    def eat(self,):
-<<<<<<< HEAD
-        self.hunger -= int(input())
-=======
+    def eat(self):
         self.hunger -= int(input("Сколько кормить? "))
->>>>>>> 285c1c5461685b5bd3f7e6d46b778b041b9cbac2
         print("Мррр...  Спасибо!")
         if self.hunger < 0:
             self.hunger = 0
         self.__pass_time()
 
     def play(self):
-<<<<<<< HEAD
-        self.boredom -= int(input())
-=======
         self.boredom -= int(input('Сколько Играть? '))
->>>>>>> 285c1c5461685b5bd3f7e6d46b778b041b9cbac2
         print("Уиии!")
         if self.boredom < 0:
             self.boredom = 0
         self.__pass_time()
-<<<<<<< HEAD
-=======
     
->>>>>>> 285c1c5461685b5bd3f7e6d46b778b041b9cbac2
     def __str__(self):
         a = "Имя зверушки: " + str(self.name) + '\n' + "Голод зверушки: " + str(self.hunger) + '\n' +  "Скука зверушки: " + str(self.boredom) + '\n'+"Настроение зверушки: " + str(self.mood) + '\n'
         return a
   
-def main():
-<<<<<<< HEAD
-    crit_name = input("Как вы назовете свою зверюшку?: ")
-    crit = Critter(crit_name)
-=======
-    
+def main(): 
     for i in range(5):
         crit_name = input("Как вы назовете свою зверюшку?: ")
-        b.append(Critter(crit_name,randint(0,10),randint(0,10)))
->>>>>>> 285c1c5461685b5bd3f7e6d46b778b041b9cbac2
+        B[crit_name] = Critter(crit_name, randint(0, 10),randint(0, 10))
 
     choice = None  
     while choice != "0":
@@ -110,10 +84,7 @@ def main():
         1 - Узнать о самочувствии зверюшки
         2 - Покормить зверюшку
         3 - Поиграть со зверюшкой
-<<<<<<< HEAD
-=======
         4 - Конкретная зверушка
->>>>>>> 285c1c5461685b5bd3f7e6d46b778b041b9cbac2
         """)
     
         choice = input("Ваш выбор: ")
@@ -122,41 +93,47 @@ def main():
         # выход
         if choice == "0":
             print("До свидания.")
-
-        # беседа со зверюшкой
+        
         elif choice == "1":
-<<<<<<< HEAD
-            crit.talk()
-        
-        # кормление зверюшки
-        elif choice == "2":
-            crit.eat()
-         
-        # игра со зверюшкой
-        elif choice == "3":
-            crit.play()
-        
-        elif choice == "secret":
-            print(crit)
-=======
-            for elem in b:
-                elem.talk()
-        
-        # кормление зверюшки
-        elif choice == "2":
-            for elem in b:
-                elem.eat()
-         
-        # игра со зверюшкой
-        elif choice == "3":
-            for elem in b:
-                elem.play()
-        
-        elif choice == "secret":
-            for elem in b:
-                 print(elem)
+            for val in B:
+                val.talk()
 
->>>>>>> 285c1c5461685b5bd3f7e6d46b778b041b9cbac2
+        elif choice == "secret":
+            for val in B:
+                print(val)
+                
+        
+        # кормление зверюшки
+        elif choice == "2":
+            for val in B:
+                val.eat()
+         
+        # игра со зверюшкой
+        elif choice == "3":
+            for val in B:
+                val.play()
+
+        elif choice == "4":
+            print('Напишите имя зверя')
+            imya = input()
+            print \
+        ("""
+        Моя зверюшка
+    
+        Кормить
+        Играть
+        """)
+            decision = input("Ваш выбор:")
+            if decision == "Кормить":
+                B[imya].eat()
+            elif decision == "Играть":
+                B[imya].play()
+            else:
+                print("Так нельзя")
+        elif choice == "secret":
+            for val in B:
+                 print(val)
+
 
         # непонятный пользовательский ввод
         else:
